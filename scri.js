3a659f206a27a8c91b7863ae7a9afb84e26d6571
Nicollebatista <nicolveiker@gmail.com>
2024-02-17 15:10:23 -0400

    first commit

---
+++ scri.js
@@ -0,0 +1,7 @@
+function cambiarColor() {
+    var elementosP = document.getElementsByTagName("p");
+    for (var i = 0; i < elementosP.length; i++) {
+      elementosP[i].style.color = "blue";
+    }
+  }
+  
